25d392c876f0b65c06c2ad6f09435eb51c531dd7
Felipe Trejos <felipetrejoslacayo2015@hotmail.com>
2026-01-13 20:12:40 +0100

    add iqkeyboardmanager as dependency

--- Arc One/AppDelegate.swift
+++ Arc One/AppDelegate.swift
@@ -2,11 +2,13 @@ import UIKit
 import FirebaseCore
 import GoogleSignIn
 import FirebaseAuth
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.isEnabled = true
         FirebaseApp.configure()
         return true
     }
